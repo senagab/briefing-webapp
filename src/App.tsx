98ef8b0f2a8fa7bab9033de2513078ea859bdfab
senagab <senagab@gmail.com>
2025-03-03 23:26:05 -0300

    feat: ListItem tag

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import ListItem from "./components/ListItem"
 
 function App() {
     const items = ['novo', 'clientes', 'sair']
-    const numbers = [1, 2, 3, 4, 5];
-    const dobro = numbers .map((numbers ) => numbers * 2);
-    console.log(dobro);
 
     return  (
         <>
             <h2>Menu</h2>
-            <ListItem text={items.map(item)} />
+            <ListItem text="item 1" />
         </>
     );
 }
