d85a9e0c445b19dd710ab583cb2a2fceda561378
senagab <senagab@gmail.com>
2025-03-11 22:49:04 -0300

    feat: novo cliente's temporary form

--- src/pages/novo.tsx
+++ src/pages/novo.tsx
@@ -2,6 +2,16 @@ const Novo = () => {
     return (
         <>
             <h2>Novo cliente</h2>
+            <form>
+                <label htmlFor="label">Client's Name</label>
+                <input type="text" />
+
+                <br />
+                
+                <label htmlFor="label">Square Meter</label>
+                <input type="number" />
+            </form>
+            <button>Start</button>
         </>
     )
 }
