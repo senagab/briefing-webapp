5081881c6097639aef985cf6227110e7abdfdab4
senagab <senagab@gmail.com>
2025-03-12 13:23:14 -0400

    feat: clientes list

--- src/pages/clientes.tsx
+++ src/pages/clientes.tsx
@@ -1,6 +1,17 @@
+import ListItem from "../components/ListItem"
+
+const items = [
+    { text: 'Cliente 01', href: '/clientes/cliente-01' },
+    { text: 'Cliente 02', href: '/clientes/cliente-02' },
+    { text: 'Cliente 03', href: '/clientes/cliente-03' },
+];
+
 const Clientes = () => {
     return (
-        <h2>Clientes</h2>
+        <>
+            <h2>Clientes</h2>
+            <ListItem items={items} />
+        </>
     )
 }
 
