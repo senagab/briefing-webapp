436374db6f77255bf181dfa800365f620055455f
senagab <senagab@gmail.com>
2025-03-20 22:02:40 -0300

    feat: protected route

---
+++ src/routes/AppRoute.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { RouteObject, useRoutes } from 'react-router-dom';
+import { publicRoutes } from './public';
+import { privateRoutes } from './private';
+import { fallbackRoute } from './fallback';
+import { ProtectedRoute } from './components';
+
+type Route = {
+    path: string;
+    element: JSX.Element;
+};
+
+export function AppRoutes() {
+    const parseRouteObjects = (
+        routes: Route[],
+        isPrivate: boolean = false
+    ): RouteObject[] => {
+        return routes.map((route) => ({
+        path: route.path,
+        element: isPrivate ? (
+            <ProtectedRoute>{route.element}</ProtectedRoute>
+        ) : (
+            route.element
+        ),
+        }));
+    };
+
+    const publicRouteObjects = parseRouteObjects(publicRoutes);
+    const privateRouteObjects = parseRouteObjects(privateRoutes);
+    const fallbackRouteObjects = parseRouteObjects(fallbackRoute);
+
+    const routes = [
+        ...publicRouteObjects,
+        ...privateRouteObjects,
+        ...fallbackRouteObjects,
+    ];
+
+    const allRoutes = useRoutes(routes);
+
+    return <React.Fragment> {allRoutes} </React.Fragment>;
+}
