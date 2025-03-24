4800b7777864986f4ee564136ad22c1b808061dc
senagab <senagab@gmail.com>
2025-03-24 20:23:09 -0300

    feat: text on notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,12 @@
 const NotFound = () => {
     
     return (
-        <h2>Page Not Found</h2>
+        <>
+            <h2>Page Not Found</h2>
+            <p>
+                Sorry to say, but the page you wanted is not available right now. Please return later.
+            </p>
+        </>
     )
 }
 
